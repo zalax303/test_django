b7647397c0f6279a96e02d2cd0ada1eda8fad5b5
Hao.Chen <hao.chen@datayes.com>
2016-05-01 20:00:41 +0800

    20160430

--- myforum/myforum/urls.py
+++ myforum/myforum/urls.py
@@ -20,9 +20,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = [
-	url(r'^admin/', include(admin.site.urls)),
-	url(r'^$', "block.views.block_list" , name="block_list"),
-	# url(r'^$', "block.views.index"),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', "block.views.block_list", name="block_list"),
+    # url(r'^$', "block.views.index"),
     url(r'^article/', include("article.urls")),
 
 
