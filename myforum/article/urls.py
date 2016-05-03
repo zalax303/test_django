bf8840e316cff8befbb8a3b433f289e9bc6336af
Hao.Chen <hao.chen@datayes.com>
2016-05-03 15:21:12 +0800

    20160503

--- myforum/article/urls.py
+++ myforum/article/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls import url
 urlpatterns = [
 	url(r'^lists/(?P<block_id>\d+)', "article.views.article_list", name="article_list"),
 	#把\d匹配的数字都传递给block_id这个变量
+	url(r'^create/(?P<block_id>\d+)', "article.views.create_article", name="create_article"),
 	]
 
--- myforum/article/views.py
+++ myforum/article/views.py
@@ -1,6 +1,8 @@
 #coding:utf-8
-from django.shortcuts import render_to_response
+from django.core.urlresolvers import reverse
+from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext  #导入模板
+from django.contrib import messages
 from block.models import Block
 from models import Article
 from django.contrib.auth.models import User
@@ -17,21 +19,23 @@ def article_list(request, block_id):  #block_id来自于url中的block_id变量
 	return render_to_response("article_list.html", {"articles": articles, "block": block},
 							context_instance=RequestContext(request))  #必加该参数
 
+
 def create_article(request, block_id):
 	block_id = int(block_id)
 	block = Block.objects.get(id=block_id)  #get方法要求只返回一个结果
 	if request.method == "GET":
 		return render_to_response("article_create.html", {"b": block},
 								context_instance=RequestContext(request))
-	else:  #POST
-		title = request.POST["title"].strip()
-		content = request.POST["content"].strip()
-		if not title or not content:
-			pass
-			# messages.add_message("article_create.html",{"b": block, "title":title, "content": content},
-			# 					 context_instance=RequestContext(request))
-		# owner = User.objects.all()[0]
-		# news_article = Article(block=block, owner=owner, content=content, title=title)
-		# news_article.save()
-		# messages.add_message(request, message.INFO, u'文章发布成功')
-		# return redirect(reverse("article_list", args=[block.id]))
+# 	else:  #POST
+# 		title = request.POST["title"].strip()
+# 		content = request.POST["content"].strip()
+# 		if not title or not content:
+# 			# pass
+# 			messages.add_message(request, messages.ERROR, u'标题和内容不允许为空')
+# 			return render_to_response("article_create.html",{"b": block, "title":title, "content": content},
+# 								 context_instance=RequestContext(request))
+# 		owner = User.objects.all()[0]  #TODO
+# 		news_article = Article(block=block, owner=owner, content=content, title=title)
+# 		news_article.save()
+# 		messages.add_message(request, messages.INFO, u'文章发布成功')
+# 		return redirect(reverse("article_list", args=[block.id])) #根据url名称反查url地址(返回一个字符串) 等同于html {% url 'URL名称' 位置参数 关键字参数 %}
