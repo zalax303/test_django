6dde68cd1de63fe9e238c2225cf17a8a69bd5361
zalax303 <chenhao_0313@hotmail.com>
2016-05-04 22:52:37 +0800

    20160504

--- myforum/usercenter/models.py
+++ myforum/usercenter/models.py
@@ -1,3 +1,12 @@
+#coding:utf-8
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ActivateCode(models.Model):
+	owner = models.ForeignKey(User, verbose_name="用户")
+	code = models.CharField(verbose_name="激活码", max_length=100)
+
+	expire_timestamp = models.DateTimeField()
+	create_time = models.DateTimeField(auto_now_add=True)
+	update_time = models.DateTimeField(auto_now=True)
--- myforum/usercenter/views.py
+++ myforum/usercenter/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+#coding:utf-8
+import datetime
+import uuid
 
-# Create your views here.
+from django.core.mail import send_mail
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.shortcuts import render_to_response,redirect
+from django.template import RequestContext
+
+from models import ActivateCode
+
