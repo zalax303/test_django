554666a959efee672a9e4c3614b93653f9bd5e9d
zalax303 <chenhao_0313@hotmail.com>
2016-05-04 22:24:05 +0800

    20160504

--- myforum/myforum/settings.py
+++ myforum/myforum/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'block',
     'article',
+    'usercenter',
 )
 
 MIDDLEWARE_CLASSES = (
--- myforum/myforum/urls.py
+++ myforum/myforum/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     url(r'^$', "block.views.block_list", name="block_list"),
     # url(r'^$', "block.views.index"),
     url(r'^article/', include("article.urls")),  #代表使用article下面的urls.py来处理
-
+    url(r'^usercenter/', include("usercenter.urls")),
 
 ]
