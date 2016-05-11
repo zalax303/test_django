590a68992b9991454ed23afa287be546824cafd5
zalax303 <chenhao_0313@hotmail.com>
2016-05-11 22:53:51 +0800

    20160511

--- myforum/article/views.py
+++ myforum/article/views.py
@@ -6,9 +6,7 @@ from django.contrib import messages
 from block.models import Block
 from models import Article
 from django.contrib.auth.models import User
-
-#
-# Create your views here.
+from django.contrib.auth.decorators import login_required
 
 
 def article_list(request, block_id):  #block_id来自于url中的block_id变量
@@ -20,6 +18,7 @@ def article_list(request, block_id):  #block_id来自于url中的block_id变量
 							context_instance=RequestContext(request))  #必加该参数
 
 
+@login_required  # 强制只有登入的用户才能发文章
 def create_article(request, block_id):
 	block_id = int(block_id)
 	block = Block.objects.get(id=block_id)  #get方法要求只返回一个结果
@@ -36,7 +35,7 @@ def create_article(request, block_id):
 								 context_instance=RequestContext(request))   #停留在当前页面
 		else:
 			owner = User.objects.all()[0]  #TODO
-			news_article = Article(block=block, owner=owner, content=content, title=title)
+			news_article = Article(block=block, owner=request.user, content=content, title=title)
 			news_article.save()
 			messages.add_message(request, messages.INFO, u'文章发布成功')
 			return redirect(reverse("article_list", args=[block.id])) #根据url名称反查url地址(返回一个字符串) 等同于html {% url 'URL名称' 位置参数 关键字参数 %}
